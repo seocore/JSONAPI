831d19a8a774ef67da35bdc782701b47f868296c
Aleksei Rapenok <alex.rapenok@gmail.com>
2025-04-08 14:18:28 +0300

    Added 21-25 Includes

--- Sources/JSONAPIKit/Document/AdditionalIncludes.swift
+++ Sources/JSONAPIKit/Document/AdditionalIncludes.swift
@@ -263,3 +263,308 @@ extension Poly20: CodablePrimaryResource, OptionalCodablePrimaryResource where
     S: CodablePolyWrapped,
     T: CodablePolyWrapped
 {}
+
+// MARK: - 21 items
+
+public typealias Include21 = Poly21
+extension Includes where I: _Poly21 {
+    public subscript(_ lookup: I.T21.Type) -> [I.T21] {
+        return values.compactMap(\.t21)
+    }
+}
+
+extension Poly21: EncodablePrimaryResource, OptionalEncodablePrimaryResource where
+    A: EncodablePolyWrapped,
+    B: EncodablePolyWrapped,
+    C: EncodablePolyWrapped,
+    D: EncodablePolyWrapped,
+    E: EncodablePolyWrapped,
+    F: EncodablePolyWrapped,
+    G: EncodablePolyWrapped,
+    H: EncodablePolyWrapped,
+    I: EncodablePolyWrapped,
+    J: EncodablePolyWrapped,
+    K: EncodablePolyWrapped,
+    L: EncodablePolyWrapped,
+    M: EncodablePolyWrapped,
+    N: EncodablePolyWrapped,
+    O: EncodablePolyWrapped,
+    P: EncodablePolyWrapped,
+    Q: EncodablePolyWrapped,
+    R: EncodablePolyWrapped,
+    S: EncodablePolyWrapped,
+    T: EncodablePolyWrapped,
+    T21: EncodablePolyWrapped
+{}
+
+extension Poly21: CodablePrimaryResource, OptionalCodablePrimaryResource where
+    A: CodablePolyWrapped,
+    B: CodablePolyWrapped,
+    C: CodablePolyWrapped,
+    D: CodablePolyWrapped,
+    E: CodablePolyWrapped,
+    F: CodablePolyWrapped,
+    G: CodablePolyWrapped,
+    H: CodablePolyWrapped,
+    I: CodablePolyWrapped,
+    J: CodablePolyWrapped,
+    K: CodablePolyWrapped,
+    L: CodablePolyWrapped,
+    M: CodablePolyWrapped,
+    N: CodablePolyWrapped,
+    O: CodablePolyWrapped,
+    P: CodablePolyWrapped,
+    Q: CodablePolyWrapped,
+    R: CodablePolyWrapped,
+    S: CodablePolyWrapped,
+    T: CodablePolyWrapped,
+    T21: CodablePolyWrapped
+{}
+
+// MARK: - 22 items
+
+public typealias Include22 = Poly22
+extension Includes where I: _Poly22 {
+    public subscript(_ lookup: I.T22.Type) -> [I.T22] {
+        return values.compactMap(\.t22)
+    }
+}
+
+extension Poly22: EncodablePrimaryResource, OptionalEncodablePrimaryResource where
+    A: EncodablePolyWrapped,
+    B: EncodablePolyWrapped,
+    C: EncodablePolyWrapped,
+    D: EncodablePolyWrapped,
+    E: EncodablePolyWrapped,
+    F: EncodablePolyWrapped,
+    G: EncodablePolyWrapped,
+    H: EncodablePolyWrapped,
+    I: EncodablePolyWrapped,
+    J: EncodablePolyWrapped,
+    K: EncodablePolyWrapped,
+    L: EncodablePolyWrapped,
+    M: EncodablePolyWrapped,
+    N: EncodablePolyWrapped,
+    O: EncodablePolyWrapped,
+    P: EncodablePolyWrapped,
+    Q: EncodablePolyWrapped,
+    R: EncodablePolyWrapped,
+    S: EncodablePolyWrapped,
+    T: EncodablePolyWrapped,
+    T21: EncodablePolyWrapped,
+    T22: EncodablePolyWrapped
+{}
+
+extension Poly22: CodablePrimaryResource, OptionalCodablePrimaryResource where
+    A: CodablePolyWrapped,
+    B: CodablePolyWrapped,
+    C: CodablePolyWrapped,
+    D: CodablePolyWrapped,
+    E: CodablePolyWrapped,
+    F: CodablePolyWrapped,
+    G: CodablePolyWrapped,
+    H: CodablePolyWrapped,
+    I: CodablePolyWrapped,
+    J: CodablePolyWrapped,
+    K: CodablePolyWrapped,
+    L: CodablePolyWrapped,
+    M: CodablePolyWrapped,
+    N: CodablePolyWrapped,
+    O: CodablePolyWrapped,
+    P: CodablePolyWrapped,
+    Q: CodablePolyWrapped,
+    R: CodablePolyWrapped,
+    S: CodablePolyWrapped,
+    T: CodablePolyWrapped,
+    T21: CodablePolyWrapped,
+    T22: CodablePolyWrapped
+{}
+
+// MARK: - 23 items
+
+public typealias Include23 = Poly23
+extension Includes where I: _Poly23 {
+    public subscript(_ lookup: I.T23.Type) -> [I.T23] {
+        return values.compactMap(\.t23)
+    }
+}
+
+extension Poly23: EncodablePrimaryResource, OptionalEncodablePrimaryResource where
+    A: EncodablePolyWrapped,
+    B: EncodablePolyWrapped,
+    C: EncodablePolyWrapped,
+    D: EncodablePolyWrapped,
+    E: EncodablePolyWrapped,
+    F: EncodablePolyWrapped,
+    G: EncodablePolyWrapped,
+    H: EncodablePolyWrapped,
+    I: EncodablePolyWrapped,
+    J: EncodablePolyWrapped,
+    K: EncodablePolyWrapped,
+    L: EncodablePolyWrapped,
+    M: EncodablePolyWrapped,
+    N: EncodablePolyWrapped,
+    O: EncodablePolyWrapped,
+    P: EncodablePolyWrapped,
+    Q: EncodablePolyWrapped,
+    R: EncodablePolyWrapped,
+    S: EncodablePolyWrapped,
+    T: EncodablePolyWrapped,
+    T21: EncodablePolyWrapped,
+    T22: EncodablePolyWrapped,
+    T23: EncodablePolyWrapped
+{}
+
+extension Poly23: CodablePrimaryResource, OptionalCodablePrimaryResource where
+    A: CodablePolyWrapped,
+    B: CodablePolyWrapped,
+    C: CodablePolyWrapped,
+    D: CodablePolyWrapped,
+    E: CodablePolyWrapped,
+    F: CodablePolyWrapped,
+    G: CodablePolyWrapped,
+    H: CodablePolyWrapped,
+    I: CodablePolyWrapped,
+    J: CodablePolyWrapped,
+    K: CodablePolyWrapped,
+    L: CodablePolyWrapped,
+    M: CodablePolyWrapped,
+    N: CodablePolyWrapped,
+    O: CodablePolyWrapped,
+    P: CodablePolyWrapped,
+    Q: CodablePolyWrapped,
+    R: CodablePolyWrapped,
+    S: CodablePolyWrapped,
+    T: CodablePolyWrapped,
+    T21: CodablePolyWrapped,
+    T22: CodablePolyWrapped,
+    T23: CodablePolyWrapped
+{}
+
+// MARK: - 24 items
+
+public typealias Include24 = Poly24
+extension Includes where I: _Poly24 {
+    public subscript(_ lookup: I.T24.Type) -> [I.T24] {
+        return values.compactMap(\.t24)
+    }
+}
+
+extension Poly24: EncodablePrimaryResource, OptionalEncodablePrimaryResource where
+    A: EncodablePolyWrapped,
+    B: EncodablePolyWrapped,
+    C: EncodablePolyWrapped,
+    D: EncodablePolyWrapped,
+    E: EncodablePolyWrapped,
+    F: EncodablePolyWrapped,
+    G: EncodablePolyWrapped,
+    H: EncodablePolyWrapped,
+    I: EncodablePolyWrapped,
+    J: EncodablePolyWrapped,
+    K: EncodablePolyWrapped,
+    L: EncodablePolyWrapped,
+    M: EncodablePolyWrapped,
+    N: EncodablePolyWrapped,
+    O: EncodablePolyWrapped,
+    P: EncodablePolyWrapped,
+    Q: EncodablePolyWrapped,
+    R: EncodablePolyWrapped,
+    S: EncodablePolyWrapped,
+    T: EncodablePolyWrapped,
+    T21: EncodablePolyWrapped,
+    T22: EncodablePolyWrapped,
+    T23: EncodablePolyWrapped,
+    T24: EncodablePolyWrapped
+{}
+
+extension Poly24: CodablePrimaryResource, OptionalCodablePrimaryResource where
+    A: CodablePolyWrapped,
+    B: CodablePolyWrapped,
+    C: CodablePolyWrapped,
+    D: CodablePolyWrapped,
+    E: CodablePolyWrapped,
+    F: CodablePolyWrapped,
+    G: CodablePolyWrapped,
+    H: CodablePolyWrapped,
+    I: CodablePolyWrapped,
+    J: CodablePolyWrapped,
+    K: CodablePolyWrapped,
+    L: CodablePolyWrapped,
+    M: CodablePolyWrapped,
+    N: CodablePolyWrapped,
+    O: CodablePolyWrapped,
+    P: CodablePolyWrapped,
+    Q: CodablePolyWrapped,
+    R: CodablePolyWrapped,
+    S: CodablePolyWrapped,
+    T: CodablePolyWrapped,
+    T21: CodablePolyWrapped,
+    T22: CodablePolyWrapped,
+    T23: CodablePolyWrapped,
+    T24: CodablePolyWrapped
+{}
+
+// MARK: - 25 items
+
+public typealias Include25 = Poly25
+extension Includes where I: _Poly25 {
+    public subscript(_ lookup: I.T25.Type) -> [I.T25] {
+        return values.compactMap(\.t25)
+    }
+}
+
+extension Poly25: EncodablePrimaryResource, OptionalEncodablePrimaryResource where
+    A: EncodablePolyWrapped,
+    B: EncodablePolyWrapped,
+    C: EncodablePolyWrapped,
+    D: EncodablePolyWrapped,
+    E: EncodablePolyWrapped,
+    F: EncodablePolyWrapped,
+    G: EncodablePolyWrapped,
+    H: EncodablePolyWrapped,
+    I: EncodablePolyWrapped,
+    J: EncodablePolyWrapped,
+    K: EncodablePolyWrapped,
+    L: EncodablePolyWrapped,
+    M: EncodablePolyWrapped,
+    N: EncodablePolyWrapped,
+    O: EncodablePolyWrapped,
+    P: EncodablePolyWrapped,
+    Q: EncodablePolyWrapped,
+    R: EncodablePolyWrapped,
+    S: EncodablePolyWrapped,
+    T: EncodablePolyWrapped,
+    T21: EncodablePolyWrapped,
+    T22: EncodablePolyWrapped,
+    T23: EncodablePolyWrapped,
+    T24: EncodablePolyWrapped,
+    T25: EncodablePolyWrapped
+{}
+
+extension Poly25: CodablePrimaryResource, OptionalCodablePrimaryResource where
+    A: CodablePolyWrapped,
+    B: CodablePolyWrapped,
+    C: CodablePolyWrapped,
+    D: CodablePolyWrapped,
+    E: CodablePolyWrapped,
+    F: CodablePolyWrapped,
+    G: CodablePolyWrapped,
+    H: CodablePolyWrapped,
+    I: CodablePolyWrapped,
+    J: CodablePolyWrapped,
+    K: CodablePolyWrapped,
+    L: CodablePolyWrapped,
+    M: CodablePolyWrapped,
+    N: CodablePolyWrapped,
+    O: CodablePolyWrapped,
+    P: CodablePolyWrapped,
+    Q: CodablePolyWrapped,
+    R: CodablePolyWrapped,
+    S: CodablePolyWrapped,
+    T: CodablePolyWrapped,
+    T21: CodablePolyWrapped,
+    T22: CodablePolyWrapped,
+    T23: CodablePolyWrapped,
+    T24: CodablePolyWrapped,
+    T25: CodablePolyWrapped
+{}
